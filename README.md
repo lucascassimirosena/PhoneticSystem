f09f7f4335164ce8e45de08447a2f78a9ba3af18
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-10-03 10:24:40 -0300

    working on phonetic system

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PhoneticSystem
  This is a little piece of software I created to practice the C language while memorizing the Phonetic System.
+
+The Phonetic System is a mnemonic technique that allows one to memorize any number using a system that converts numbers into images.
+
+I have it memorized by heart but to put my knowledge of C language in practice I'll create a system that would help me to memorize it just for the fun.
---
+++ main.c
@@ -0,0 +1,10 @@
+/* 
+
+First, I'll create the alghorithm itself, thinking and structuring it in a logic way that will help me to build it.
+
+The idea will be the software show a random number OR word that is part of my phonetic system. It will show the random number or word and I must answer it correctly 
+according to my own Phonetic System. 
+
+It means that I'll need a rand function that will compare the generated number to a number located in a vector and return the correct or incorrect statement.
+
+*/
